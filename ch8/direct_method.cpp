468e06b004c58f2f2a67e35425cfd233b118ac21
tomkimsour <tomkimsour@gmail.com>
2023-02-23 13:50:13 +1030

    compile ch7 & ch8

--- ch8/direct_method.cpp
+++ ch8/direct_method.cpp
@@ -4,6 +4,7 @@
 #include <pangolin/pangolin.h>
 
 using namespace std;
+using namespace cv;
 
 typedef vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>> VecVector2d;
 
@@ -203,7 +204,7 @@ void DirectPoseEstimationSingleLayer(
 
     // plot the projected pixels here
     cv::Mat img2_show;
-    cv::cvtColor(img2, img2_show, CV_GRAY2BGR);
+    cv::cvtColor(img2, img2_show, COLOR_GRAY2BGR);
     VecVector2d projection = jaco_accu.projected_points();
     for (size_t i = 0; i < px_ref.size(); ++i) {
         auto p_ref = px_ref[i];
